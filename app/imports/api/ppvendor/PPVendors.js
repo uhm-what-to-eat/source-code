cbb3a58bfe4dddea651d9c2d30efdec4c6fd938a
John Paul Alonzo <93769010+Johnzo1233@users.noreply.github.com>
2024-04-11 13:45:18 -1000

    fixing the hours

--- app/imports/api/ppvendor/PPVendors.js
+++ app/imports/api/ppvendor/PPVendors.js
@@ -15,46 +15,12 @@ class PPVendorsCollection {
       name: String,
       image: String,
       location: String,
+      monday: String,
+      tuesday: String,
+      wednesday: String,
+      thursday: String,
+      friday: String,
       owner: String,
-      hours: {
-        type: Object,
-        label: 'Opening hours for each day of the week',
-      },
-      'hours.monday': {
-        type: String,
-        label: 'Monday',
-        optional: true, // Optional depending on your requirements
-      },
-      'hours.tuesday': {
-        type: String,
-        label: 'Tuesday',
-        optional: true,
-      },
-      'hours.wednesday': {
-        type: String,
-        label: 'Wednesday',
-        optional: true,
-      },
-      'hours.thursday': {
-        type: String,
-        label: 'Thursday',
-        optional: true,
-      },
-      'hours.friday': {
-        type: String,
-        label: 'Friday',
-        optional: true,
-      },
-      'hours.saturday': {
-        type: String,
-        label: 'Saturday',
-        optional: true,
-      },
-      'hours.sunday': {
-        type: String,
-        label: 'Sunday',
-        optional: true,
-      },
     });
     // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
     this.collection.attachSchema(this.schema);
