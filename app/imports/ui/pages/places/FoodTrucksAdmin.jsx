751797186d19762f9e9aa98c4bcfee43d1046b7c
joshuanssato <156635500+joshuanssato@users.noreply.github.com>
2024-04-11 19:11:47 -1000

    Issue 23 Partly Done Campus Center Remove works

--- app/imports/ui/pages/places/FoodTrucksAdmin.jsx
+++ app/imports/ui/pages/places/FoodTrucksAdmin.jsx
@@ -5,10 +5,10 @@ import { Col, Container, Row, Button, Nav } from 'react-bootstrap';
 import { useTracker } from 'meteor/react-meteor-data';
 import { FTVendors } from '../../../api/ftvendor/FTVendors';
 import LoadingSpinner from '../../components/LoadingSpinner';
-import PlaceToEat from '../../components/PlaceToEat';
+import PlaceToEatAdmin from '../../components/PlaceToEatAdmin';
 
 /* Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
-const FoodTrucks = () => {
+const FoodTrucksAdmin = () => {
   // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
   const { ready, ftvendor } = useTracker(() => {
     // Note that this subscription will get cleaned up
@@ -29,32 +29,32 @@ const FoodTrucks = () => {
       <Row className="justify-content-center py-3">
         <Col>
           <Col className="text-center">
-            <h2 className="fw-bold">Food Trucks</h2>
+            <h2 className="fw-bold">Food Trucks Vendors</h2>
           </Col>
         </Col>
       </Row>
       <Row className="text-center">
         <Col>
           <Button variant="success" className="m-1">
-            <Nav.Link as={NavLink} to="/cc">Campus Center</Nav.Link>
+            <Nav.Link as={NavLink} to="/adminCC">Campus Center Vendors</Nav.Link>
           </Button>
           <Button variant="success" className="m-1">
-            <Nav.Link as={NavLink} to="/pp">Paradise Palms</Nav.Link>
+            <Nav.Link as={NavLink} to="/adminPP">Paradise Palms Vendors</Nav.Link>
           </Button>
-          <Button variant="success" className="m-1" disabled>Food Trucks</Button>
+          <Button variant="success" className="m-1" disabled>Food Trucks Vendors</Button>
           <Button variant="success" className="m-1">
-            <Nav.Link as={NavLink} to="/hh">Hemenway Hall</Nav.Link>
+            <Nav.Link as={NavLink} to="/hh">Hemenway Hall Vendors</Nav.Link>
           </Button>
           <Button variant="success" className="m-1">
-            <Nav.Link as={NavLink} to="/rd">Residential Dining</Nav.Link>
+            <Nav.Link as={NavLink} to="/rd">Residential Dining Vendors</Nav.Link>
           </Button>
         </Col>
       </Row>
       <Row xs={1} md={2} lg={3} className="g-4 py-4">
-        {ftvendor.map((place) => (<Col key={place._id}><PlaceToEat place={place} /></Col>))}
+        {ftvendor.map((place) => (<Col key={place._id}><PlaceToEatAdmin place={place} /></Col>))}
       </Row>
     </Container>
   ) : <LoadingSpinner />);
 };
 
-export default FoodTrucks;
+export default FoodTrucksAdmin;
