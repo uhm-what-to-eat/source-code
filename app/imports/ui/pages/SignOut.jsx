c598bf057af3756798e7b6d7a5e8a4c3f7bc9f60
Christian Dela Cruz <cdc21@hawaii.edu>
2024-05-03 21:32:59 -1000

    issue-75
    changed fonts, created spinning image, and changed sign out page

--- app/imports/ui/pages/SignOut.jsx
+++ app/imports/ui/pages/SignOut.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Col } from 'react-bootstrap';
+import { Col, Image, Container } from 'react-bootstrap';
 
 /* After the user clicks the "SignOut" link in the NavBar, log them out and display this page. */
 const SignOut = () => {
@@ -10,9 +10,16 @@ const SignOut = () => {
   }, []);
 
   return (
-    <Col id="signout-page" className="text-center py-3">
-      <h2>You are signed out.</h2>
-    </Col>
+    <Container fluid className="py-3">
+      <Col />
+      <div className="text-center">
+        <Image src="https://cdn.dribbble.com/users/3951514/screenshots/7288432/media/866b49d81c982fdecd5a22de44c5e677.gif" className="signout-image" />
+        <Col id="signout-page">
+          <h1>Safe Travels and Happy Eating!</h1>
+        </Col>
+      </div>
+    </Container>
+
   );
 };
 
